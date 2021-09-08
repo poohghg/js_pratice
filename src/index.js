3843b728357d433716a90a024ba95332381ed5a9
poohghg <38716228+poohghg@users.noreply.github.com>
2021-09-08 14:41:05 +0900

    commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,45 @@
 import "./styles.css";
 // max
+
+const array1 = [1, 2, 3, 4, 5, 6];
+const array2 = [1, 2, 3, 4, 5];
+
+// restToArray(1, 2, 3, 4, 10, 5, 6, 7);
+// max(1, 2, 3, 4, 10, 5, 6, 7);
+// arrayToString(["a", "b", "c"]);
+// arraySplice(array1);
+arraySlice(array2);
+
+// 배열로 파라메터를 받아옴
+function restToArray(...rest) {
+  console.log("restToArray", rest);
+}
+
 function max(...rest) {
-  console.log(rest);
   const result = rest.reduce((prev, curr) => (curr > prev ? curr : prev));
+  console.log("max-result : ", result);
   return result;
 }
 
-const result = max(1, 2, 3, 4, 10, 5, 6, 7);
-console.log(result);
+function arrayToString(rest) {
+  console.log("arrayToString", rest.join());
+  return rest.join();
+}
 
-// 테스트 코드에서 불러오기 위하여 사용하는 코드
-export default max;
+function arraySplice(array1) {
+  const array = array1;
+  // splice는 원본 배열을 변경하여 리턴
+  console.log("array : ", array);
+  console.log("splice : ", array.splice(3));
+  console.log("arraySplice : ", array);
+}
+
+function arraySlice(array1) {
+  const array = array1;
+  console.log("array : ", array);
+  console.log("slice : ", array.slice(3));
+  console.log("arraySlice : ", array);
+}
+
+function testApp() {}
+export default testApp;
