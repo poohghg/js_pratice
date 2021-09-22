c79c350c8073bdfe9b2db5aad5f4226ccc295f6e
khg <poohghg@naver.com>
2021-09-22 21:06:23 +0900

    commit

--- src/app.js
+++ src/app.js
@@ -8,25 +8,44 @@ function sendAjax(method,url) {
     return JSON.parse(ajax.response);
 }
 
+const store = {
+    currentPage : 1,
+    totalPageNum : 0
+}
+
 function newFeed() {
     const feedUrl = "https://api.hnpwa.com/v0/news/1.json"; 
     const newsFeeds = sendAjax("GET",feedUrl);
-
+    store.totalPageNum = newsFeeds.length % 10 === 0 ? newsFeeds.length/10 : newsFeeds.length/10 + 1;
     const newList = [];
     newList.push(`<ul>`)
-    newsFeeds.map(feed =>{
+    
+    for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
         /**
          * Dom Api을 사용하지 않고 문자열만을 가지고 DOM을 만들기
          */
         newList.push(`
-        <li>
-            <a href=#${feed.id}> 
-                ${feed.title} (${feed.comments_count})
-            </a>    
-        </li>
+            <li>
+                <a href="#/show/${newsFeeds[i].id}"> 
+                    ${newsFeeds[i].title} (${newsFeeds[i].comments_count})
+                </a>    
+            </li>
         `);
-    })
+    }
+    
     newList.push(`</ul>`)
+    newList.push(`
+        <div>
+            <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+            <a href="#/page/${store.currentPage === store.totalPageNum 
+                ? store.totalPageNum 
+                : store.currentPage + 1}
+            ">
+                다음 페이지
+            </a>
+            <a href="#/page/1">홈</a>
+        </div>
+    `)
     // join은 배열을 문자열로 리턴
     container.innerHTML = newList.join('');
 }
@@ -35,7 +54,7 @@ function newDetail() {
     const contentDiv = document.createElement("div");
     contentDiv.className = "content"
     // 해쉬 체이지 이벤트는 일종의 북마크가 변경된것
-    const id = location.hash.replace("#","")
+    const id = location.hash.replace("#/show/","")
     const contentUrl = `https://api.hnpwa.com/v0/item/${id}.json`;
     const content = sendAjax("GET",contentUrl);
 
@@ -43,31 +62,24 @@ function newDetail() {
     container.innerHTML = `
         <h1>${content.title}</h1>
         <div>
-            <a href=#>목록으로</a>
+            <a href="#/page/${store.currentPage}">목록으로</a>
         </div>
     `;
-
-    // if(contentDiv.hasChildNodes("h1")){
-    //     const title = document.querySelector(".content > h1");
-    //     title.innerText = content.title;
-    // }else{
-    //     const title = document.createElement("h1");
-    //     title.innerText = content.title;
-    //     contentDiv.appendChild(title);
-    // }
 }
 
 function router() {
     const routePath = location.hash;
     // location hash에 #만있을경우 빈문자열로 간주한다.
-    console.log("routePath",routePath)
     if (routePath === '') {
+        // 빈페이일 경우
         newFeed();
-    } else {
+    } else if(routePath.indexOf("#/page/") > -1){
+        store.currentPage = Number(location.hash.split("#/page/")[1]);
+        newFeed();
+    } else if(routePath.indexOf("#/show/") > -1) {
         newDetail();
     }
 }
 
-
 window.addEventListener('hashchange',router);
-newFeed();
+router();
