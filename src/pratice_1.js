15201ec9f9e127b2848a69aac3e1be0a998d5421
poohghg <38716228+poohghg@users.noreply.github.com>
2022-05-09 17:01:01 +0900

    commit

--- src/pratice_1.js
+++ src/pratice_1.js
@@ -12,4 +12,19 @@ function unaryFuncionts() {
   console.log(result, xx);
 }
 
+function checkTypeOf() {
+  console.log("null ", typeof null);
+  console.log("isFunction ? ", typeof unaryFuncionts);
+  // null은 object로 반환한다. js의 버그이다.
+  // type of 는  원시타입5개 + object + function 총 7개 형태로 반환한다.
+  // null 타입은 반환하지 않는다.
+
+  const temp = null;
+  // temp 는 object다 ㅡㅡ
+  console.log("temp의 타입은 ? ", typeof temp);
+  console.log(typeof temp === null);
+  console.log(temp === null);
+}
+
 unaryFuncionts();
+checkTypeOf();
